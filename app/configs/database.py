a32a0fa44e9d8f9c7b553638421bf7f4a87e8c57
Тимофей Бешкуров <beshkurov@ashmanov.net>
2022-10-17 00:24:47 +0300

    path fix

--- app/configs/database.py
+++ app/configs/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os 
 
+
+os.makedirs(os.path.join("app", "static"), exist_ok=True)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app/static/blog.db"
 
 engine = create_engine(
